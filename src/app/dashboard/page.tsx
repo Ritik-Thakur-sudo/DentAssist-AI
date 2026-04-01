4095a7af7522fa068fe9d1f4fe562bea4e9837a4
Ritik Thakur <rk7495933@gmail.com>
2026-04-02 00:28:30 +0530

    create dashboard for admin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,22 @@
+import ActivityOverview from '@/components/dashboard/ActivityOverview';
+import MainActions from '@/components/dashboard/MainActions';
+import WelcomeSection from '@/components/dashboard/WelcomeSection';
 import Navbar from '@/components/Navbar';
 import React from 'react'
 
 
 function DashboardPage() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+
+      </div>
+    </>
   )
 }
 
