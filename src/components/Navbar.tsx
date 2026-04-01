7fb76bc4cabe3af5e3271fff04c3d87c0b225da9
Ritik Thakur <rk7495933@gmail.com>
2026-04-01 18:04:21 +0530

    add voice assistant using vapi and voice page added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,9 +14,9 @@ function Navbar() {
     <nav className="fixed top-0 left-0 right-0 z-50 px-6 py-2 border-b border-border/50 bg-background/80 backdrop-blur-md h-16">
       <div className="max-w-7xl mx-auto flex justify-between items-center h-full">
         {/* LOGO */}
-        <div className="flex items-center gap-8">
+        <div className="flex items-center gap-9">
           <Link href="/dashboard" className="flex items-center gap-2">
-            <Image src="/logo.png" alt="DentWise Logo" width={32} height={32} className="w-11" />
+            <Image src="/logo.png" alt="DentAssist Logo" width={32} height={32} className="w-10" />
           </Link>
 
           <div className="flex items-center gap-6">
@@ -61,7 +61,7 @@ function Navbar() {
 
         {/* RIGHT SECTION */}
         <div className="flex items-center gap-4">
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-4">
             <div className="hidden lg:flex flex-col items-end">
               <span className="text-sm font-medium text-foreground">
                 {user?.firstName} {user?.lastName}
